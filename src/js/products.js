4a34cc8df4de36e4dfb9016c48c768287ece10b7
vitorvf <vitorferreiravf2@gmail.com>
2021-10-08 13:43:23 -0300

    update-mobile

--- src/js/products.js
+++ src/js/products.js
@@ -1,3 +1,26 @@
 import '../scss/desktop-content.scss';
 
 
+var btn = document.querySelector("#btn");
+btn.addEventListener("click", function() {
+    var div = document.querySelector("#filtro");
+    
+  if(div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+      div.style.display = "none";
+  }
+    
+});
+
+var btn = document.querySelector("#btnselecionar");
+btn.addEventListener("click", function() {
+    var div = document.querySelector("#selecao");
+    
+  if(div.style.display === "none") {
+        div.style.display = "block";
+    } else {
+      div.style.display = "none";
+  }
+    
+});
